a9a1b2d22d0894cb29fae759ae36a240f1a1e773
aallison <aallison@yahoo-inc.com>
2014-06-04 20:54:22 -0700

    initial commit

--- SwiftHello/ViewController.swift
+++ SwiftHello/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var helloLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloLabel = UILabel(frame: CGRect(x:100.0, y:100.0, width:200.0, height:40.0))
+        helloLabel.text = "Hello, World!"
+        
+        self.view.addSubview(helloLabel!)
     }
 
     override func didReceiveMemoryWarning() {
